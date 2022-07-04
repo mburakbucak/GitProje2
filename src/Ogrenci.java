38bcd5371726ff73af38ac2031dd113fc6ee5ee1
mburakbucak <mburakbucak@gmail.com>
2022-07-04 21:48:09 +0300

    Öğrenci Classı ikinci bölüm yapıldı

--- src/Ogrenci.java
+++ src/Ogrenci.java
@@ -1,5 +1,8 @@
 public class Ogrenci {
     public static void main(String[] args) {
-        System.out.println("merhaba dünya");
+
+
+        System.out.println("Merhaba Dünya 2");
+        System.out.println("Merhaba Dünya 3");
     }
 }
